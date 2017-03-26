2ba22056d7643b36437d85dede6cb8e28c7d456b
Dror Russin <benadam1988@walla.com>
2017-03-26 17:36:38 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,3 +1,12 @@
 import csv
 import sqlite3
 
+def create_csv(file):
+    conn = sqlite3.connect('example.db')
+    conn.text_factory = str 
+    cur = conn.cursor()
+    data = cur.execute("SELECT * FROM txt"+str(file))
+    with open('history'+str(file)+'.csv', 'w') as new:
+        writer = csv.writer(new)
+        writer.writerow(['Temprature','Humidity', 'Date','Time'])
+        writer.writerows(data)
