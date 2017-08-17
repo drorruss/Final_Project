29148dfd37751df4bb11c09546587a75e84fe9af
Dror Russin <benadam1988@walla.com>
2017-08-17 17:46:18 +0300

    updating

--- src/new.py
+++ src/new.py
@@ -3,6 +3,7 @@ import csv
 import Adafruit_DHT as dht
 import datetime
 from twilio.rest import TwilioRestClient
+import run_gui as run
     
 def main():   
    file = open('data'+'.txt', 'w+')
@@ -11,7 +12,7 @@ def main():
    conn = sqlite3.connect('example.db')
    cur = conn.cursor()
    cur.execute('''CREATE TABLE IF NOT EXISTS data 
-                (Temprature text,Humidity text,Date text, Time text)''')
+                (Temprature text,Humidity text,Date text, Time text, Audio text, Pulse text)''')
    var = 1
    while var <= 2 :
        h,t = dht.read_retry(dht.DHT22, 4)
@@ -22,9 +23,11 @@ def main():
        time =  now.strftime("%H:%M:%S")
        file.write ('Date=' + d)
        file.write (' Time=' + time)
-       file.write('\n')   
-       purchases = [(t,h,d,time)]
-       cur.executemany('INSERT INTO data VALUES (?,?,?,?)',purchases)
+       file.write('\n')
+       aud='0'
+       pul='0'
+       purchases = [(t,h,d,time,aud,pul)]
+       cur.executemany('INSERT INTO data VALUES (?,?,?,?,?,?)',purchases)
        conn.commit()
        check_exe(h,t)
        var = var + 1
@@ -33,7 +36,7 @@ def main():
    dd = cur.execute("SELECT * FROM data")
    with open('history.csv', 'w') as f:
        writer = csv.writer(f)
-       writer.writerow(['Temprature','Humidity','Date','Time'])
+       writer.writerow(['Temprature','Humidity','Date','Time','Audio','Pulse'])
        writer.writerows(dd)
 
            
@@ -42,21 +45,35 @@ def main():
 
 
 def check_exe(h,t):
-   if (t >35 and t < 36.6) or (t < 38 and t > 37.2):
-     print "Temprature Exeption - Please go to the baby !!"    
-   if (t < 35 or t > 38):
-     print ("Temprature Exeption - Please go rapidly to the baby !! ")  
-   if (h > 25 and h < 30) or (h > 50 and h < 55):
-     print ("Humidity Exeption - Please go to the baby !!")  
-   if (h < 25 or h > 55):
+   if (t >= 25):
+     print ("Temprature Exeption - Please go rapidly to the baby !! ")
+     # send_msg_to_phone()
+     run.show_label()
+   if (t <21 or t > 23) : # modify according to the environment, age
+     print "Temprature Exeption - Please go to the baby !!"
+     #send_msg_to_phone()
+   if (h < 30):
      print ("Humidity Exeption - Please go rapidly to the baby !!")
+     #send_msg_to_phone()
+   if (h > 60 or h < 40):
+     print ("Humidity Exeption - Please go to the baby !!")
+     #send_msg_to_phone()
+   
 
-
+def show_label():
+        self.label.show()
    
 def send_msg_to_phone():
     account_sid = "AC6c0dbc8ae5b4ca4624001b3b4ea75cda"
     auth_token = "f585e28caf77a953e2fcdf57ab1f13c1"
     client = TwilioRestClient(account_sid, auth_token)
-    message = client.messages.create(to="+972527906248", from_="+19163785052",
-                                         body="Hello there!")
+    message = client.messages.create(to="+972502560520", from_="+19163785052",
+                                         body="the Temprature not good in the baby room please go to check")
+
+
+
+def open_contact():
+   file2 = open('contact'+'.txt', 'w+')
+   file2.write('My contact List')
+
 
